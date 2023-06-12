f9c0cfcfc56071e65e61d83b8d1f89580130ea12
dreinicke27 <danicareinicke@gmail.com>
2023-06-12 14:50:05 -0700

    Added Restaurant and RestaurantList components

---
+++ src/components/Restaurant.js
@@ -0,0 +1,16 @@
+import "./Restaurant.css"
+
+const Restaurant = () => {
+    return (
+        <div>
+        <h2 className="restaurant-title">Salty's</h2>
+        <ul>
+            <li>Cuisine: Seafood</li>
+            <li>Rating: 4.8</li>
+            <li>Distance from Ada: 7.5</li>
+        </ul>
+        </div>
+    );
+};
+
+export default Restaurant;
---
+++ src/components/RestaurantList.js
@@ -0,0 +1,13 @@
+import Restaurant from "./Restaurant";
+
+const RestaurantList = () => {
+    return (
+        <div>
+            <h1>Restaurant List</h1>
+            <Restaurant />
+            <Restaurant />
+        </div>
+    );
+};
+
+export default RestaurantList;
